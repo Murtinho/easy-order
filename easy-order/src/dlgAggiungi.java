456594893e1ddbe0a71fc0030f39240dff3751d3
Alessandro Murtas <alessandro.murtas@galileo.galileicrema.it>
2022-05-26 12:47:46 +0200

    Completato dialog aggiungi

--- easy-order/src/dlgAggiungi.java
+++ easy-order/src/dlgAggiungi.java
@@ -28,63 +28,84 @@ public class dlgAggiungi extends javax.swing.JDialog {
 
         jPanel1 = new javax.swing.JPanel();
         lblNome = new javax.swing.JLabel();
-        jTextField1 = new javax.swing.JTextField();
+        txtNome = new javax.swing.JTextField();
         lblDescrizione = new javax.swing.JLabel();
-        jTextField2 = new javax.swing.JTextField();
+        txtDescrizione = new javax.swing.JTextField();
         lblAllergeni = new javax.swing.JLabel();
-        jTextField3 = new javax.swing.JTextField();
+        txtAllergeni = new javax.swing.JTextField();
         lblPrezzo = new javax.swing.JLabel();
-        jTextField4 = new javax.swing.JTextField();
+        txtPrezzo = new javax.swing.JTextField();
+        lblCategoria = new javax.swing.JLabel();
+        cbxCategoria = new javax.swing.JComboBox<>();
+        btnAnnulla = new javax.swing.JButton();
+        btnAggiungi = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        setTitle("Aggiungi");
 
-        jPanel1.setLayout(new java.awt.GridLayout());
+        jPanel1.setLayout(new java.awt.GridLayout(6, 2, 10, 10));
 
         lblNome.setText("Nome");
         jPanel1.add(lblNome);
-        jPanel1.add(jTextField1);
+        jPanel1.add(txtNome);
 
         lblDescrizione.setText("Descrizione");
         jPanel1.add(lblDescrizione);
-        jPanel1.add(jTextField2);
+        jPanel1.add(txtDescrizione);
 
         lblAllergeni.setText("Allergeni");
         jPanel1.add(lblAllergeni);
-        jPanel1.add(jTextField3);
+        jPanel1.add(txtAllergeni);
 
         lblPrezzo.setText("Prezzo");
         jPanel1.add(lblPrezzo);
-        jPanel1.add(jTextField4);
+        jPanel1.add(txtPrezzo);
+
+        lblCategoria.setText("Categoria");
+        jPanel1.add(lblCategoria);
+
+        cbxCategoria.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Antipasti", "Primi", "Secondi", "Pizze", "Dessert", "Bevande" }));
+        jPanel1.add(cbxCategoria);
+
+        btnAnnulla.setText("Annulla");
+        jPanel1.add(btnAnnulla);
+
+        btnAggiungi.setText("Aggiungi");
+        jPanel1.add(btnAggiungi);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(128, 128, 128)
+                .addContainerGap()
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(103, 103, 103)
+                .addContainerGap()
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(175, Short.MAX_VALUE))
+                .addGap(0, 15, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnAggiungi;
+    private javax.swing.JButton btnAnnulla;
+    private javax.swing.JComboBox<String> cbxCategoria;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JTextField jTextField1;
-    private javax.swing.JTextField jTextField2;
-    private javax.swing.JTextField jTextField3;
-    private javax.swing.JTextField jTextField4;
     private javax.swing.JLabel lblAllergeni;
+    private javax.swing.JLabel lblCategoria;
     private javax.swing.JLabel lblDescrizione;
     private javax.swing.JLabel lblNome;
     private javax.swing.JLabel lblPrezzo;
+    private javax.swing.JTextField txtAllergeni;
+    private javax.swing.JTextField txtDescrizione;
+    private javax.swing.JTextField txtNome;
+    private javax.swing.JTextField txtPrezzo;
     // End of variables declaration//GEN-END:variables
 }
