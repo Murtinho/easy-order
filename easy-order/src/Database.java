6d5cc1938e37c2f2c660b45a65379ae2a26133ee
Alessandro Murtas <alessandro.murtas@galileo.galileicrema.it>
2022-05-29 18:16:10 +0200

    Inserite le credenziali per la connessione al db

--- easy-order/src/Database.java
+++ easy-order/src/Database.java
@@ -2,12 +2,15 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
+import java.sql.*;
 /**
  *
  * @author aless
  */
 public class Database
 {
-    
+    private final static String URL = "jdbc:mysql://192.168.1.242:3306/easy?";
+    private final static String USER = "remote";
+    private final static String PASSWORD = "password";
+    //private final static String DRIVER = "com.mysql.cj.jdbc.Driver";
 }
