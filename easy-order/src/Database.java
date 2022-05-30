8ff32cdccf903220afee0c20bd767a275485c39a
Alessandro Murtas <alessandro.murtas@galileo.galileicrema.it>
2022-05-30 20:41:39 +0200

    Creata funzione login
    restituisce true se il login è avvenuto

--- easy-order/src/Database.java
+++ easy-order/src/Database.java
@@ -3,14 +3,41 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 /**
  *
  * @author aless
  */
 public class Database
 {
-    private final static String URL = "jdbc:mysql://192.168.1.242:3306/easy?";
+    private final static String URL = "jdbc:mysql://raspberry-murtas.ddns.net:3306/easy?";
     private final static String USER = "remote";
     private final static String PASSWORD = "password";
     //private final static String DRIVER = "com.mysql.cj.jdbc.Driver";
+    
+    /**
+     * @param username the username
+     * @param password the password
+     * @return true if credentials are correct, otherwise false
+     */
+    public static boolean login(String username, String password)
+    {
+        try(Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
+            PreparedStatement pstmt = conn.prepareStatement("SELECT username, password FROM users WHERE username = ? AND password = ?");)
+        {
+            pstmt.setString(1, username);
+            pstmt.setString(2, password);
+            
+            ResultSet res = pstmt.executeQuery();
+            
+            return res.next();
+        }
+        catch(SQLException ex)
+        {
+            Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return false;
+    }
 }
