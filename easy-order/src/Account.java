a1a3f44863694a4f92293f5bc3f18a28fffd1f5f
Alessandro Murtas <alessandro.murtas@galileo.galileicrema.it>
2022-05-30 21:48:29 +0200

    Creata classe account

---
+++ easy-order/src/Account.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author aless
+ */
+public class Account
+{
+    private static String USERNAME;
+    private static String PASSWORD;
+    private static int TAVOLI;
+    private static String RISTORANTE;
+    
+
+    public static String getUSERNAME() {
+        return USERNAME;
+    }
+
+    public static String getPASSWORD() {
+        return PASSWORD;
+    }
+
+    public static int getTAVOLI() {
+        return TAVOLI;
+    }
+
+    public static String getRISTORANTE() {
+        return RISTORANTE;
+    }
+}
