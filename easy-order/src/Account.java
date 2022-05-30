802f4d4b589bad83c944f96abf576d441eed569c
Alessandro Murtas <alessandro.murtas@galileo.galileicrema.it>
2022-05-30 22:22:57 +0200

    Aggiunti setter

--- easy-order/src/Account.java
+++ easy-order/src/Account.java
@@ -14,20 +14,42 @@ public class Account
     private static int TAVOLI;
     private static String RISTORANTE;
     
+    public static void salvaAccount()
+    {
+        
+    }
 
     public static String getUSERNAME() {
         return USERNAME;
+    }
+
+    public static void setUSERNAME(String USERNAME) {
+        Account.USERNAME = USERNAME;
     }
 
     public static String getPASSWORD() {
         return PASSWORD;
     }
 
+    public static void setPASSWORD(String PASSWORD) {
+        Account.PASSWORD = PASSWORD;
+    }
+
     public static int getTAVOLI() {
         return TAVOLI;
+    }
+
+    public static void setTAVOLI(int TAVOLI) {
+        Account.TAVOLI = TAVOLI;
     }
 
     public static String getRISTORANTE() {
         return RISTORANTE;
     }
+
+    public static void setRISTORANTE(String RISTORANTE) {
+        Account.RISTORANTE = RISTORANTE;
+    }
+    
+    
 }
