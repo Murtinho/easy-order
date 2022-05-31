1dfb379b7e7a3bd117e158cd3e96dedd9c32a027
Alessandro Murtas <alessandro.murtas@galileo.galileicrema.it>
2022-05-31 18:47:58 +0200

    Funzione registrazione utente
    La funzione inserisce un nuovo utente nel database con i parametri passati

--- easy-order/src/Database.java
+++ easy-order/src/Database.java
@@ -11,7 +11,7 @@ import java.util.logging.Logger;
  */
 public class Database
 {
-    private final static String URL = "jdbc:mysql://127.0.0.1:3306/easy?";
+    private final static String URL = "jdbc:mysql://raspberry-murtas.ddns.net:3306/easy?";
     private final static String USER = "remote";
     private final static String PASSWORD = "password";
     //private final static String DRIVER = "com.mysql.cj.jdbc.Driver";
@@ -32,6 +32,24 @@ public class Database
             
             if(res.next())
                 Account.setAccount(res);
+        }
+        catch(SQLException ex)
+        {
+            Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public static void register(String username, String password, int tavoli, String ristorante)
+    {
+        try(Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
+            PreparedStatement pstmt = conn.prepareStatement("INSERT INTO users (username, password, tavoli, ristorante) VALUES (?, ?, ?, ?)");)
+        {
+            pstmt.setString(1, username);
+            pstmt.setString(2, password);
+            pstmt.setInt(3, tavoli);
+            pstmt.setString(4, ristorante);
+            
+            pstmt.executeUpdate();
         }
         catch(SQLException ex)
         {
